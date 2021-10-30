a0872ab43ed118cb868e786a4c5528693115790e
bonjunku <ku96kbj@naver.com>
2021-10-30 15:46:37 +0900

    :sparkles: feat : add headers, search function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,27 @@
 import React, { useEffect, useState } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAkA1qhALOkcbjffIVlwlpf2gjBHCaudcA`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log('error', error));
+  };
 
   //배열이 비었으면 최초 마운트 시에만 호출.
   useEffect(() => {
@@ -21,7 +39,12 @@ function App() {
       .catch((error) => console.log('error', error));
   }, []);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
